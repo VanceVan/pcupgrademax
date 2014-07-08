6f15abcdc7f91ba01a88c29461cc077e951ace31
VanceVan <vaughn@radsoft.com>
2014-07-08 05:37:07 -0700

    PC UPGRADE HAS BEEN BORN

--- web.rb
+++ web.rb
@@ -12,8 +12,8 @@ get '/about.html' do
   send_file 'about.html'
 end
 
-get '/jquery-1.10.1.js' do
-  send_file 'jquery-1.10.1.js'
+get '/jquery.js' do
+  send_file 'jquery.js'
 end
 
 get '/stylesheet.css' do
